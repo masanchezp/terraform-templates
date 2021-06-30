bc22501118ed63799444b3cca06f7268788f11f3
masanchezp <45571264+masanchezp@users.noreply.github.com>
2021-06-30 14:46:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,17 +71,7 @@ resource azurerm_network_security_group "bad_sg" {
   name                = "terragoat-${var.environment}"
   resource_group_name = azurerm_resource_group.example.name
 
-  security_rule {
-    access                     = "Allow"
-    direction                  = "Inbound"
-    name                       = "AllowSSH"
-    priority                   = 200
-    protocol                   = "TCP"
-    source_address_prefix      = "*"
-    source_port_range          = "*"
-    destination_port_range     = "22-22"
-    destination_address_prefix = "*"
-  }
+  
 
   security_rule {
     access                     = "Allow"
