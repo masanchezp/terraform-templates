a2bea83232611b99f113b92ca6fe5c2e4999b389
masanchezp <45571264+masanchezp@users.noreply.github.com>
2022-12-05 17:01:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,32 @@
 resource "azurerm_storage_account" "example" {
-  name = "example"
-  resource_group_name = azurerm_resource_group.example.name
+  name                     = "tgsa${var.environment}${random_integer.rnd_int.result}"
+  resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-  blob_public_access = "true"   <-----         
+  blob_public_access = "true"   <-----
+  queue_properties {
+    logging {
+      delete                = false
+      read                  = false
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+  tags = {
+    
+  }
 }
